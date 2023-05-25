c513e758ce79052c02c8b785a80e000d28868f5f
Guilherme Ávila <90806129+guilhermeavila77@users.noreply.github.com>
2023-05-24 22:42:34 -0300

    Inserção área de projetos

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//Codigo da cordialidade
 function titulo(periodo){
     const cabeca = document.querySelector('.titulo');
     return cabeca.innerHTML = `${periodo} meu nome é Guilherme!`
@@ -19,3 +20,35 @@ function defineperiodo(hora){
 
 const periodoAtual = defineperiodo(pegaHora());
 titulo(periodoAtual);
+
+// Codigo responsavel pelo scroll
+const container = document.querySelector('.container');
+let scrollLine = document.querySelector('.scroll');
+
+container.addEventListener('wheel', (e)=> {
+    e.preventDefault();
+    container.scrollLeft += e.deltaY;
+    scrollLine.style.width = container.scrollLeft / 2 + 'px';
+})
+
+
+//Escrevendo progetos
+
+//Funções Titulo
+function projTitulo(page, projeto){
+    const tituloProj = document.querySelector(page)
+    return tituloProj.innerHTML = projeto
+}
+projTitulo('.page_1', 'TCC engenharia')
+projTitulo('.page_2', 'IOT')
+projTitulo('.page_3', 'Lista de tarefas')
+/*
+//Funçoes para os textos
+function projTexto(page, texto){
+    const tituloProj = document.querySelector(page)
+    return tituloProj.innerHTML += texto
+}
+
+projTexto('text_page_1', 'Em desenvolvimento,')
+projTexto('text_page_2')
+projTexto('text_page_3')*/
